e87d4817b27ad41c93e09946e065231bc2eec3e9
student <damian@pollub.edu.pl>
2023-10-05 15:53:40 +0200

    feat/pollub-3

---
+++ src/main/java/org/example/Role.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Role {
+    public void addRole(String name) {
+        System.out.println(name);
+    }
+
+    public void deleteRole(String name) {
+        System.out.println(name);
+    }
+}
